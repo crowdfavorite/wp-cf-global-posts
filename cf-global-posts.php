7213ec19d810b8a7cdd8a3165f44ef224837bd6e
mrichmond <mrichmond@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-10-21 20:08:23 +0000

    Version updating for tag, and updating of changelog.txt
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-global-posts/trunk@17430 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-global-posts.php
+++ cf-global-posts.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Global Posts 
 Plugin URI: http://crowdfavorite.com
 Description: Generates a 'shadow blog' where posts mu-install-wide are conglomorated into one posts table for fast data compilation and retrieval.
-Version: 1.6(trunk)
+Version: 1.6
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
@@ -802,7 +802,7 @@ function cfgp_operations_form() {
 				<h4><?php _e('Click the button below to set up the Global Blog', 'cf-global-posts'); ?></h4>
 				<form method="post">
 					<input type="hidden" name="cf_action" value="cfgp_setup_shadow_blog" />
-					<button class="button-primary" type="submit"><?php _e('Setup Global Blog Now', 'cf-global-posts'); ?></button>
+					<button class="button-primary" type="submit"><?php _e('Set up Global Blog Now', 'cf-global-posts'); ?></button>
 				</form>
 				<?php
 			}
