1eb29da6204713615f6a843cca2c867986d0f8d1
Matt Richmond <matt@crowdfavorite.com>
2012-02-17 10:46:21 -0700

    Breaking out views for admin page

---
+++ views/import-table-ajax-receipt.php
@@ -0,0 +1,4 @@
+<div id="doing-import">
+	<h3></h3>
+	<p id="import-ticks"></p>
+</div>
---
+++ views/install.php
@@ -0,0 +1,6 @@
+<h3><?php _e('Global Blog has not been setup','cf-global-posts'); ?></h3>
+<h4><?php _e('Click the button below to set up the Global Blog', 'cf-global-posts'); ?></h4>
+<form method="post">
+	<input type="hidden" name="cf_action" value="cfgp_setup_shadow_blog" />
+	<button class="button-primary" type="submit"><?php _e('Set up Global Blog Now', 'cf-global-posts'); ?></button>
+</form>
